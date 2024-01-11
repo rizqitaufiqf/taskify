e767bfce15578aae792377a2030405427c981cf4
Rizqi Taufiq <rizqitaufiqf@gmail.com>
2024-01-11 11:46:53 +0700

    feat: root navbar (header)

--- src/app/(marketing)/layout.tsx
+++ src/app/(marketing)/layout.tsx
@@ -1,9 +1,10 @@
 import { PropsWithChildren } from "react";
+import { Navbar } from "./_components/navbar";
 
 const MarketingLayout = ({ children }: PropsWithChildren) => {
   return (
     <div className="h-full bg-slate-100">
-      {/* Navbar */}
+      <Navbar />
       <main className="bg-slate-100 pb-20 pt-40">{children}</main>
       {/* Footer */}
     </div>
