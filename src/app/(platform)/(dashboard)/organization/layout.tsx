a19eca0fdf57eda4f46b1e9325b4d8af4f7f5340
Rizqi Taufiq <rizqitaufiqf@gmail.com>
2024-01-12 10:12:54 +0700

    feat: organization settings page

--- src/app/(platform)/(dashboard)/organization/layout.tsx
+++ src/app/(platform)/(dashboard)/organization/layout.tsx
@@ -5,7 +5,7 @@ const OrganizationLayout = ({ children }: PropsWithChildren) => {
   return (
     <div className="mx-auto max-w-6xl px-4 pt-20 md:pt-24 2xl:max-w-screen-xl">
       <div className="flex gap-x-7">
-        <div className="hidden w-64 shrink-0 md:block">
+        <div className="hidden w-64 shrink-0 lg:block">
           <Sidebar />
         </div>
         {children}
