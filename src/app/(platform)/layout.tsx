184848d0e00862510e8d54596003a0a19573056c
Rizqi Taufiq <rizqitaufiqf@gmail.com>
2024-01-13 13:32:58 +0700

    feat: add toast in create task

--- src/app/(platform)/layout.tsx
+++ src/app/(platform)/layout.tsx
@@ -1,8 +1,14 @@
 import { ClerkProvider } from "@clerk/nextjs";
 import { PropsWithChildren } from "react";
+import { Toaster } from "sonner";
 
 const PlatformLayout = ({ children }: PropsWithChildren) => {
-  return <ClerkProvider>{children}</ClerkProvider>;
+  return (
+    <ClerkProvider>
+      <Toaster />
+      {children}
+    </ClerkProvider>
+  );
 };
 
 export default PlatformLayout;
--- src/components/form/form-popover.tsx
+++ src/components/form/form-popover.tsx
@@ -13,6 +13,7 @@ import {
 import { useAction } from "@/hooks/use-action";
 import { X } from "lucide-react";
 import React from "react";
+import { toast } from "sonner";
 
 interface FormPopoverProps {
   children: React.ReactNode;
@@ -30,9 +31,11 @@ export const FormPopover = ({
   const { execute, fieldErrors } = useAction(createBoard, {
     onSuccess: (data) => {
       console.log(data);
+      toast.success("Board created");
     },
     onError: (error) => {
       console.log(error);
+      toast.error(error);
     },
   });
 
