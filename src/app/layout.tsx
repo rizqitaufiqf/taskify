3b2c97b4ccbf8eae627ee24bc152473b1faec64e
Rizqi Taufiq <rizqitaufiqf@gmail.com>
2024-01-13 15:18:51 +0700

    chore: move config folder to src

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
+import { siteConfig } from "@/config/site";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { PropsWithChildren } from "react";
-import { siteConfig } from "../../config/site";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
