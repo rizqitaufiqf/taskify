f59816f3d5293149e8b210792a978b196ebfb145
Rizqi Taufiq <rizqitaufiqf@gmail.com>
2024-01-13 12:27:55 +0700

    refactor: delete test file for form component

--- src/app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ src/app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,20 +1,5 @@
-import { Board } from "@/app/(platform)/(dashboard)/organization/[organizationId]/board";
-import { Form } from "@/app/(platform)/(dashboard)/organization/[organizationId]/form";
-import { db } from "@/lib/db";
-
 const OrganizationIdPage = async () => {
-  const boards = await db.board.findMany();
-
-  return (
-    <div className="flex flex-col space-y-4">
-      <Form />
-      <div className="space-y-2">
-        {boards.map((board) => (
-          <Board key={board.id} title={board.title} id={board.id} />
-        ))}
-      </div>
-    </div>
-  );
+  return <div className="mb-20 w-full"></div>;
 };
 
 export default OrganizationIdPage;
