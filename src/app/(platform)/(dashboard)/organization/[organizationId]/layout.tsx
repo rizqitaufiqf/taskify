ac7660a06a18b8f6b5946060690280229d1b6d13
Rizqi Taufiq <rizqitaufiqf@gmail.com>
2024-01-15 12:28:30 +0700

    feat: add current organization on title metadata

--- src/app/(platform)/(dashboard)/organization/[organizationId]/layout.tsx
+++ src/app/(platform)/(dashboard)/organization/[organizationId]/layout.tsx
@@ -1,5 +1,15 @@
 import { OrgControl } from "@/app/(platform)/(dashboard)/organization/[organizationId]/_components/org-control";
+import { auth } from "@clerk/nextjs";
+import { startCase } from "lodash";
 import { PropsWithChildren } from "react";
+
+export async function generateMetadata() {
+  const { orgSlug } = auth();
+
+  return {
+    title: startCase(orgSlug || "organization"),
+  };
+}
 
 const OrganizationIdLayout = ({ children }: PropsWithChildren) => {
   return (
