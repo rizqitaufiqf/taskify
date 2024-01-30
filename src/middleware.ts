a61804da60dc78a52d8c933828c2fdb5babca6cd
Rizqi Taufiq <rizqitaufiqf@gmail.com>
2024-01-30 14:11:40 +0700

    feat: add stripe webhook

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,7 +5,7 @@ import { NextResponse } from "next/server";
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
 export default authMiddleware({
-  publicRoutes: ["/"],
+  publicRoutes: ["/", "/api/webhook"],
   afterAuth(auth, req) {
     // if user is login, redirect to select-org or if user is selected an org, redirect to dashboard organization
     if (auth.userId && auth.isPublicRoute) {
